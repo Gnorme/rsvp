890a4e06121f2237923420969e256fb2ec4d11ef
Gnorme <thompsoncwt@gmail.com>
2023-05-12 16:35:27 -0400

    accordian effect

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function showOptions() {
   if (document.getElementById("attending").checked) {
     console.log("CHECKED");
     document.getElementById("dinner-choice").style.maxHeight = "300px";
+    document.getElementById("dinner-choice").style.paddingTop = "20px";
   } else {
     document.getElementById("dinner-choice").style.maxHeight = "0px";
+    document.getElementById("dinner-choice").style.paddingTop = "0px";
   }
 }
