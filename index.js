7603f3db44947c0e461f53ea114ebc71215efc23
Gnorme <thompsoncwt@gmail.com>
2023-05-12 19:32:33 -0400

    border

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ function showOptions() {
     document.getElementById("dinner-choice").style.paddingTop = "0px";
   }
 }
+
+function checkBorder() {
+  if (document.getElementById("check-border").checked) {
+    document.getElementById("rsvp-form").style.border = "3px solid";
+    document.getElementById("rsvp-form").style.borderImageSlice = "1";
+  } else {
+    document.getElementById("rsvp-form").style.border = "none";
+  }
+}
